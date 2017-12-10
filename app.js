8e10530a22f526809b57fed1f7e291d506c87dfb
Smelt <shivamsatyarthi@gmail.com>
2017-12-10 01:36:00 -0500

    failed attempt

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
-var request = require('request');
-request('http://www.google.com', function (error, response, body) {
-    console.log('error:', error); // Print the error if one occurred
-    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    console.log('body:', body); // Print the HTML for the Google homepage.
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+//tells express to serve our public folder
+app.use(express.static("public"));
+var friends = ["Tony", "Miranda", "Adam", "Barb"];
+//tells app that we don't need to specify we are using eJS files
+app.set("view engine", "ejs");
+
+app.get("/", function (req, res) {
+    res.render("home.ejs");
 });
+
+app.post("/addfriend", function (req, res) {
+    console.log(req.body);
+    var newFriend = req.body.newFriend;
+    friends.push(newFriend);
+    res.redirect("/friends");
+    res.send("You have reached the post Route!!");
+})
+
+//friends
+app.get("/friends", function (req, res) {
+
+    res.render("friends", { friends: friends });
+})
+
+app.listen(3000, function () {
+    console.log("Server is listening");
+})
